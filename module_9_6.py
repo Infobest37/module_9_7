6a3e825d1fe12726a4b0f39162f1f5eab342a608
Евгений <info-best@bk.ru>
2024-10-07 19:47:50 +0300

    second

--- module_9_6.py
+++ module_9_6.py
@@ -1,23 +1,28 @@
 
-
-
 def is_prime(func):
     def wrapper(*args, **kwargs):
         num = func(*args, **kwargs)
-        if isinstance(num, int):
-            return "Простое"
+        j = 1
+        for i in range(1, num):
+
+            if num % i == 0:
+                j += 1
+        if j > 2:
+            return(f"Делений: {j} поэтому число - Простое ")
         else:
-            return "Составное"
+            return (f"Делений: {j} поэтому число - Составное ")
+
+
+
     return wrapper
 
 
 @is_prime
 def sum_three(*n):
-
     print(sum(n))
     return sum(n)
 
 
 
-result = sum_three(2, 3, 6)
+result = sum_three(2)
 print(result)
